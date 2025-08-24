63aec3fb4969a6ba867b662cef478f83993c4c7d
wonderfulgkh <42545883+wonderfulgkh@users.noreply.github.com>
2025-08-24 19:05:37 +0800

    Delete 个人引导页_带后台 directory

--- 个人引导页_带后台/admin/static/js/index.js
+++
@@ -1,8 +0,0 @@
- // 显示错误信息
-        function showError(message) {
-            const errorElement = document.getElementById('errorMessage');
-            errorElement.textContent = message;
-            errorElement.style.display = 'block';
-        }
-        
-      
